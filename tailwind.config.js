dd1a721b19f56f1099005d7caee91dfa8c3a241b
Steven Woodward <steveanthony999@gmail.com>
2024-03-16 11:34:20 -0700

    Defined colors and applied to elements

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.html'],
+  content: ["./**/*.html"],
   theme: {
+    colors: {
+      primary: "#141414",
+      secondary: "#1F1F1F",
+      tertiary: "#333333",
+      light: "#FFFFFF",
+      link: "#444444",
+      accent: "#C4F82A",
+    },
     extend: {},
   },
   plugins: [],
